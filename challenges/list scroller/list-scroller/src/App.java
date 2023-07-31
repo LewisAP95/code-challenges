9fc9129a1469d805c652f96203804a9a1f07bb54
Lewis Perry <LewisA.Perry@hotmail.co.uk>
2023-07-31 14:03:39 +0100

    List scroller challenge starting.

---
+++ challenges/list scroller/list-scroller/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+//Given two strings A and B, return whether or not A can be shifted some number of times to get B.
+//For example, if A is abcde and B is cdeab, return true. If A is abc and B is acb, return false.
+
+    public static void main(String[] args) throws Exception {
+        String stringA = "abcde";
+        String stringB = "cdeab";
+    }
+
+    public boolean scrollTest(String stringA, String stringB){
+        return true;
+    }
+}

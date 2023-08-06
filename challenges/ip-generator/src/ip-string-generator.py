14972788e655a0c6564ce20afdd70822ee9240b7
Lewis Perry <LewisA.Perry@hotmail.co.uk>
2023-08-06 16:02:31 +0100

    Further refactor, compressed into one code block.

--- challenges/ip-generator/src/ip-string-generator.py
+++ challenges/ip-generator/src/ip-string-generator.py
@@ -10,30 +10,26 @@ def main():
     if len(source_digits) not in range(4,13):
         raise SystemExit("Starting string either too long or too short to form a complete IP address.")
     
-    new_ip_blocks = []
     for first_block in range(1,4):
         for second_block in range(1, 4):
             for third_block in range(1, 4):
                 for fourth_block in range(1, 4):
                     block = [first_block, second_block, third_block, fourth_block]
                     if sum(block) == len(source_digits):
-                        new_ip_blocks.append(block)
+                        new_ip = []
+                        slice_offset = 0
+                        invalid = False
+                        for i in block:
+                            ip_slice = source_digits[slice_offset:i+slice_offset]
+                            if int(ip_slice) not in range(0, 256) or (len(ip_slice) > 1 and ip_slice[0] == "0"):
+                                invalid = True
+                            else:
+                                new_ip.append(ip_slice)
+                            slice_offset += i
 
-    for ip_block in new_ip_blocks:
-        new_ip = []
-        slice_offset = 0
-        invalid = False
-        for i in ip_block:
-            ip_slice = source_digits[slice_offset:i+slice_offset]
-            if int(ip_slice) not in range(0, 256) or (len(ip_slice) > 1 and ip_slice[0] == "0"):
-                invalid = True
-            else:
-                new_ip.append(ip_slice)
-            slice_offset += i
-
-        if not invalid:
-            ip_string = f"{new_ip[0]}.{new_ip[1]}.{new_ip[2]}.{new_ip[3]}"
-            new_ips.append(ip_string)
+                        if not invalid:
+                            ip_string = f"{new_ip[0]}.{new_ip[1]}.{new_ip[2]}.{new_ip[3]}"
+                            new_ips.append(ip_string)
 
     print(new_ips)
 

d4a092e7eb0c1cba629eddef1ef673faad6c76f3
Lewis Perry <LewisA.Perry@hotmail.co.uk>
2023-10-24 13:33:15 +0100

    Matrix island finder project creation.

---
+++ challenges/matrix-island-finder/matrix-island-finder/src/App.java
@@ -0,0 +1,15 @@
+//Given a matrix of 1s and 0s, return the number of "islands" in the matrix.
+// A 1 represents land and 0 represents water, so an island is a group of 1s that are neighboring whose perimeter is surrounded by water.
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[][] islandMatrix = {
+            {1, 0, 0, 0, 0},
+            {0, 0, 1, 1, 0},
+            {0, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0},
+            {1, 1, 0, 0, 1},
+            {1, 1, 0, 0, 1}};
+        System.out.println("Hello, World!");
+    }
+}

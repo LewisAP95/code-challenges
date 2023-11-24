e4c7443fcb1cb6adfdd472e0417274b3a254addf
Lewis Perry <LewisA.Perry@hotmail.co.uk>
2023-11-24 04:39:43 +0000

    Addition of proper return and commenting.

--- challenges/two-string-distance/two_string_distance.py
+++ challenges/two-string-distance/two_string_distance.py
@@ -8,31 +8,34 @@ def __main__():
     second_word = "world"
 
     finder = string_distance_finder()
-    finder.find_distance(input_string, first_word, second_word)
+    print(f"Smallest amount of words between '{first_word}' and '{second_word}' was {finder.find_distance(input_string, first_word, second_word)}")
 
 class string_distance_finder():
     
     def find_distance(self, input_string, first_word, second_word):
+        # Turn the input string into a list and set up empty lists to contain the indices for the found words
         input_list = input_string.split()
-
         first_indices = []
         second_indices = []
+
+        # Starting gap is set to the length of the list as no gap could ever be longer than this
         lowest_gap = len(input_list)
 
+        # Loops through the list recording the positions of matches to either chosen word
         for i in range(len(input_list)):
             if input_list[i] == first_word:
                 first_indices.append(i)
             elif input_list[i] == second_word:
                 second_indices.append(i)
 
+        # Compares the indices against each other and returns the distance between the closest pair
+        # Does not count pairs that 'cross over' i.e. where word two is before word one
         for a in first_indices:
             for b in second_indices:
                 if (b - a) - 1 < lowest_gap and (b - a) - 1 >= 0:
                     lowest_gap = (b - a) - 1
 
-        print(first_indices)
-        print(second_indices)
-        print(lowest_gap)
+        return lowest_gap
 
 if __name__ == "__main__":
     __main__() 

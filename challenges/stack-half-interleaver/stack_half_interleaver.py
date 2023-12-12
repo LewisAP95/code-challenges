7db870b719a4b01be0b7ad04106d5e45c9a3675e
Lewis Perry <LewisA.Perry@hotmail.co.uk>
2023-12-12 17:15:45 +0000

    Stack half-interleaver base setup.

---
+++ challenges/stack-half-interleaver/stack_half_interleaver.py
@@ -0,0 +1,15 @@
+#Given a stack of N elements, interleave the first half of the stack with the second half reversed using only one other queue. This should be done in-place.
+#Recall that you can only push or pop from a stack, and enqueue or dequeue from a queue.
+#For example, if the stack is [1, 2, 3, 4, 5], it should become [1, 5, 2, 4, 3]. If the stack is [1, 2, 3, 4], it should become [1, 4, 2, 3].
+
+def __main__():
+    test_stack_one = {1, 2, 3, 4 ,5}
+    test_stack_two = {1, 2, 3, 4}
+
+class stack_half_interleaver():
+    
+    def interleave(self, input_stack):
+        working_queue = {}
+
+if __name__ == "__main__":
+    __main__()
